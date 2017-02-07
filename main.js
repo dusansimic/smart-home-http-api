9c7a6e22a2dbc45cf768b5e891d60c1093161b26
Dušan Simić <dusan.simic1810@gmail.com>
2017-02-07 18:26:31 +0100

    separating api and web server

--- main.js
+++ main.js
@@ -1,40 +1,61 @@
-var express = require('express');
-var app = express();
+// creating api server
+var api_express = require('express');
+var api_app = api_express();
+
+// creating web server
+var web_express = require('express');
+var web_app = web_express();
+var web_server = require('http').Server(web_app);
+var web_io = require('socket.io')(web_server);
+
 var jsonfile = require('jsonfile');
 
 var database = 'data/database.json';
 
-app.get('/get-temperature', function(req, res) {
-  jsonfile.readFile(database, function(err, obj) {
-    if (err === null) {
-      res.send(obj.temperature);
-    } else {
-      conosle.error(err);
-      res.send('not ok');
-    }
-  });
-});
+// === api code ===
 
-app.get('/new-temperature', function(req, res) {
+api_app.get('/new-temperature', function(req, res) {
   var newTemperature = req.query.temperature;
   jsonfile.readFile(database, function(err, obj) {
     if (err === null) {
       obj.temperature = newTemperature;
       jsonfile.writeFile(database, obj, function(err) {
         if (err !== null) {
-          console.error(err);
-          res.send('not ok');
+          console.error('[api] ' + err);
+          res.send('not ok: ' + err);
         } else {
           res.send('ok');
         }
       });
     } else {
-      console.error(err);
-      res.send('not ok');
+      console.error('[api] ' + err);
+      res.send('not ok: ' + err);
     }
   });
 });
 
-app.listen(3000, '0.0.0.0', function() {
-  console.log('listening *:3000');
+api_app.listen(3003, '0.0.0.0', function() {
+  console.log('listening api *:3003');
+});
+
+
+// === web code ===
+
+web_app.use(web_express.static('public'));
+
+web_server.listen(3000, '0.0.0.0', function() {
+  console.log('listening web *:3000');
+});
+
+web_io.on('connection', function(socket) {
+  socket.on('get temperature', function() {
+    jsonfile.readFile(database, function(err, obj) {
+      if (err === null) {
+        socket.emit('new temperature', obj.temperature);
+      } else {
+        console.error('[web] ' + err)
+        socket.emit('new temperature', err);
+      }
+    });
+  });
 });
